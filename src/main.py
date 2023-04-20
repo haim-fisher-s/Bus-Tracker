d08f19feb31a91aed43525ac5ce82e7d40359784
Fisher <haim.fisher@intel.com>
2023-04-20 21:53:09 +0300

    [repo maintenance] change workflow file to the valid name workflows and add main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+"""
+This module contains the main function of "Bus Tracker".
+"""
+
+from datetime import datetime
+
+
+def main():
+    """
+    empty function.
+    """
+    return 0
+
+
+if __name__ == "__main__":
+    start = datetime.now()
+    main()
+    print(f"Bus tracker done. Duration: {datetime.now() - start}")
