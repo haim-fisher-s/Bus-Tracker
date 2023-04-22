3769a75dfe788fcd7d57650bd51d648ea221b766
Fisher <haim.fisher@intel.com>
2023-04-23 00:12:37 +0300

    [conv LSTM] init module

---
+++ src/BTlibs/modules/conv_lstm.py
@@ -0,0 +1,49 @@
+"""
+conv_lstm is a module that uses Convolution-al LSTM architecture
+to predict the future of a time series.
+
+Contains:
+    - ConvLSTMInputBuilder
+    - ConvLSTMModel
+"""
+
+
+class ConvLSTMInputBuilder:
+    """
+    This class is used to structure input for training/predicting
+    the ConvLSTMModel class.
+    """
+
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self):
+        """
+        Initializes ConvLSTMInputBuilder class.
+        """
+
+    @staticmethod
+    def start():
+        """
+        Starts ConvLSTM data contraction process.
+        """
+        print("ConvLSTM data-contracting started")
+
+
+class ConvLSTMModel:
+    """
+    This class is used to structure the ConvLSTM model.
+    """
+
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self):
+        """
+        Initializes ConvLSTMModel class.
+        """
+
+    @staticmethod
+    def start():
+        """
+        Starts ConvLSTM model.
+        """
+        print("ConvLSTM model started")
