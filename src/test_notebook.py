bb7aaeec592e74078276944e9730296e4eed0f1f
Fisher <haim.fisher@intel.com>
2023-04-24 00:30:49 +0300

    [repo maintenance] improve-ci-test

--- src/test_notebook.py
+++ src/test_notebook.py
@@ -1,39 +1,49 @@
-"""
-This module contains the main function of "Bus Tracker".
-"""
-
 import os
 import subprocess
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 from jupyter_client.kernelspec import NoSuchKernel
 
-subprocess.run(
-    [
-        "python",
-        "-m",
-        "ipykernel",
-        "install",
-        "--user",
-        "--name",
-        "python3",
-        "--display-name",
-        "Python 3",
-    ]
-)
+
+def install_kernel_spec():
+    """
+    Install the kernel spec for Python 3 in the current user's Jupyter kernels directory.
+    """
+    subprocess.run(
+        [
+            "python",
+            "-m",
+            "ipykernel",
+            "install",
+            "--user",
+            "--name",
+            "python3",
+            "--display-name",
+            "Python 3",
+        ]
+    )
 
 
-# Path to the notebook to test
-notebook_path = "src/Bus_Tracker.ipynb"
+def check_notebook_existence(notebook_path):
+    """
+    Check if the given notebook path exists.
 
-if os.path.exists(notebook_path):
-    print("File exists")
-else:
-    print("File does not exist")
+    Args:
+    - notebook_path (str): the path to the notebook file
+
+    Returns:
+    - bool: True if the file exists, False otherwise
+    """
+    return os.path.exists(notebook_path)
 
 
-def test_notebook():
-    "test the BUS Tracker notebook"
+def execute_notebook(notebook_path):
+    """
+    Execute the given notebook file.
+
+    Args:
+    - notebook_path (str): the path to the notebook file
+    """
     with open(notebook_path) as f:
         nb = nbformat.read(f, as_version=4)
 
@@ -46,6 +56,20 @@ def test_notebook():
         assert False, f"Error executing the notebook: {e}"
 
 
+def test_notebook():
+    """
+    Test the Bus Tracker notebook by executing it and checking for errors.
+    """
+    notebook_path = "src/Bus_Tracker.ipynb"
+    if not check_notebook_existence(notebook_path):
+        print("Notebook file does not exist.")
+        return
+
+    execute_notebook(notebook_path)
+    print("Notebook executed successfully.")
+
+
 if __name__ == "__main__":
+    install_kernel_spec()
     test_notebook()
-    print("Everything passed")
+    print("Test passed.")
