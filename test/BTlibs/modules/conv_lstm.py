dc76dd554c9a1fe0c3a56065bd85bdfef2688dd9
davidfis <haim.fisher@intel.com>
2023-08-06 21:14:55 +0300

    clean repo

---
+++ test/BTlibs/modules/conv_lstm.py
@@ -0,0 +1,91 @@
+"""
+conv_lstm is a module that uses Convolution-al LSTM architecture
+to predict the future of a time series.
+
+Contains:
+    - ConvLSTMInputBuilder
+    - ConvLSTMModel
+"""
+
+
+class ConvLSTMInputBuilder:
+    """
+    This class is used to structure input for training/predicting
+    the ConvLSTMModel class.
+    """
+
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self):
+        """
+        Initializes ConvLSTMInputBuilder class.
+        """
+
+    @staticmethod
+    def fetch_and_pars_data():
+        """
+        Prepare the data for ConvLSTM model.
+        """
+        print("place holder")
+
+    @staticmethod
+    def prepare_4D_tensors():
+        """
+        Prepare the 4D tensors to train/test the model.
+        """
+        print("place holder")
+
+    @staticmethod
+    def get_train_4D_tensors():
+        """
+        Get the 4D tensors to train model.
+        """
+        print("place holder")
+
+    @staticmethod
+    def get_test_4D_tensors():
+        """
+        Get the 4D tensors to test the model.
+        """
+        print("place holder")
+
+
+class ConvLSTMModel:
+    """
+    This class is used to structure the ConvLSTM model.
+    """
+
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self):
+        """
+        Initializes ConvLSTMModel class.
+        """
+
+    @staticmethod
+    def build_model():
+        """
+        Build ConvLSTM model.
+        """
+        print("place holder")
+
+    @staticmethod
+    def train():
+        """
+        Train the ConvLSTM model.
+        """
+        print("place holder")
+
+    @staticmethod
+    def evaluate():
+        """
+        Evaluate the ConvLSTM model.
+        """
+        print("place holder")
+
+    @staticmethod
+    def predict():
+        """
+        Predict using ConvLSTM model.
+        """
+        print("place holder")
---
+++ test/test_notebook.py
@@ -0,0 +1,75 @@
+import os
+import subprocess
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+from jupyter_client.kernelspec import NoSuchKernel
+
+
+def install_kernel_spec():
+    """
+    Install the kernel spec for Python 3 in the current user's Jupyter kernels directory.
+    """
+    subprocess.run(
+        [
+            "python",
+            "-m",
+            "ipykernel",
+            "install",
+            "--user",
+            "--name",
+            "python3",
+            "--display-name",
+            "Python 3",
+        ]
+    )
+
+
+def check_notebook_existence(notebook_path):
+    """
+    Check if the given notebook path exists.
+
+    Args:
+    - notebook_path (str): the path to the notebook file
+
+    Returns:
+    - bool: True if the file exists, False otherwise
+    """
+    return os.path.exists(notebook_path)
+
+
+def execute_notebook(notebook_path):
+    """
+    Execute the given notebook file.
+
+    Args:
+    - notebook_path (str): the path to the notebook file
+    """
+    with open(notebook_path) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    os.chdir(os.path.join(os.getcwd(), "src"))
+
+    ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+    try:
+        ep.preprocess(nb)
+    except Exception as e:
+        assert False, f"Error executing the notebook: {e}"
+
+
+def test_notebook():
+    """
+    Test the Bus Tracker notebook by executing it and checking for errors.
+    """
+    notebook_path = "src/Bus_Tracker.ipynb"
+    if not check_notebook_existence(notebook_path):
+        print("Notebook file does not exist.")
+        return
+
+    execute_notebook(notebook_path)
+    print("Notebook executed successfully.")
+
+
+if __name__ == "__main__":
+    install_kernel_spec()
+    test_notebook()
+    print("Test passed.")
